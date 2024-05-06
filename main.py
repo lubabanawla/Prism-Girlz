357bf4ec844d4675ed8ad3a3257871a2f2cb86ee
Lubaba Nawla <lubabanawla@gmail.com>
2024-05-06 09:24:52 -0400

    Add files via upload
    I researched how to add videos to Pygame. Although I’m still confused on how I will go about this, I found a bunch of good resources for when I initiate the video (context: I want a video to play before the game runs).
    Here are the links:
    https://www.youtube.com/watch?v=Xu8SLkvFq8I
    https://www.youtube.com/watch?v=K8PoK3533es
    https://www.youtube.com/watch?v=zV3FalI1VOA
    
    Other than that, I initialized a background and my player class just to set things up.

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hello!!!")
+import pygame
+from player import Player
+
+pygame.init()
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Prism Girlz")
+
+# bg image
+bg_image = pygame.image.load("backgroundimage.jpeg")
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
+# create two instances of fighter
+player_1 = Player(200, 310)
+player_2 = Player(700, 310)
+
+#game loop
+run = True
+
+while run:
+
+    draw_bg()
+
+    # draw players
+    player_1.draw(screen)
+    player_2.draw(screen)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()

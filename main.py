3aa65b00d17ac7ff3f7b4abd9d7797c2a4067d27
Lubaba Nawla <lubabanawla@gmail.com>
2024-05-29 22:34:39 -0400

    Add files via upload
    I GOT THE SPRITES TO WORK!! Both player one and player two are currently animating the idle animation. Woohoo! There’s still an issue with screen restriction like the image of the sprite can go off the screen but it’s not technically going off the screen because of the red rectangle. It’s hard to put into words, but you can see it and see why there’s this weird screen restriction now. I also have to fix the flipping for player two because it kind of messes up the attacking if I try fixing it. But that’s besides the point, I got the sprites to work!! Woohoo! I’m planning to fix the weird screen restriction and flipping tomorrow, and then work on attacking animations and stuff.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 from player import Player
-import spritesheet
 
 pygame.init()
 
@@ -30,29 +29,46 @@ pygame.display.set_icon(pygame_icon)
 
 # player variables for spritesheet
 player_size = 292
-player_scale = 4
-player_offset = [72, 56]
+player_scale = 1
+player_offset = [10, 15]
 player_data = [player_size, player_scale, player_offset]
-
 
 # bg image
 bg_image = pygame.image.load("prismgirlz.png")
 
-# load sprite sheets
-playeroneatksheet = spritesheet.spritesheet("assets/images/spritesheets/playerone/playeroneatk.png")
-playeroneidlesheet = spritesheet.spritesheet("assets/images/spritesheets/playerone/playeroneidle.png")
-playeronerunsheet = spritesheet.spritesheet("assets/images/spritesheets/playerone/playeronerun.png")
-playeronespritesheet = [playeroneidlesheet, playeronerunsheet, playeroneatksheet]
-# animation
-playeronesteps = [5, 6, 5]
+# load player one images
+p1_atk_1 = pygame.image.load("assets/images/playerone/atk/Blue_ATK_1.png").convert_alpha()
+p1_atk_2 = pygame.image.load("assets/images/playerone/atk/Blue_ATK_2.png").convert_alpha()
+p1_atk_3 = pygame.image.load("assets/images/playerone/atk/Blue_ATK_3.png").convert_alpha()
+p1_atk_4 = pygame.image.load("assets/images/playerone/atk/Blue_ATK_4.png").convert_alpha()
+p1_atk_5 = pygame.image.load("assets/images/playerone/atk/Blue_ATK_5.png").convert_alpha()
+player_one_atk = [p1_atk_1, p1_atk_2, p1_atk_3, p1_atk_4, p1_atk_5]
 
-playertwoidlesheet = pygame.image.load("assets/images/spritesheets/playertwo/playertwoidle.png").convert_alpha()
-playertwospritesheet = [playertwoidlesheet, playeronerunsheet, playeroneatksheet]
-playertwosteps = [5, 6, 5]
+p1_idle_1 = pygame.image.load("assets/images/playerone/idle/Blue_Idle_1.png").convert_alpha()
+p1_idle_2 = pygame.image.load("assets/images/playerone/idle/Blue_Idle_2.png").convert_alpha()
+p1_idle_3 = pygame.image.load("assets/images/playerone/idle/Blue_Idle_3.png").convert_alpha()
+p1_idle_4 = pygame.image.load("assets/images/playerone/idle/Blue_Idle_4.png").convert_alpha()
+p1_idle_5 = pygame.image.load("assets/images/playerone/idle/Blue_Idle_5.png").convert_alpha()
+player_one_idle = [p1_idle_1, p1_idle_2, p1_idle_3, p1_idle_4, p1_idle_5]
+
+p1_run_1 = pygame.image.load("assets/images/playerone/run/Blue_Run_1.png").convert_alpha()
+p1_run_2 = pygame.image.load("assets/images/playerone/run/Blue_Run_2.png").convert_alpha()
+p1_run_3 = pygame.image.load("assets/images/playerone/run/Blue_Run_3.png").convert_alpha()
+p1_run_4 = pygame.image.load("assets/images/playerone/run/Blue_Run_4.png").convert_alpha()
+p1_run_5 = pygame.image.load("assets/images/playerone/run/Blue_Run_5.png").convert_alpha()
+p1_run_6 = pygame.image.load("assets/images/playerone/run/Blue_Run_6.png").convert_alpha()
+player_one_run = [p1_run_1, p1_run_2, p1_run_3, p1_run_4, p1_run_5, p1_run_6]
+
+# load player two images
+p2_idle_1 = pygame.image.load("assets/images/playertwo/idle/Pink_Idle_1.png").convert_alpha()
+p2_idle_2 = pygame.image.load("assets/images/playertwo/idle/Pink_Idle_2.png").convert_alpha()
+p2_idle_3 = pygame.image.load("assets/images/playertwo/idle/Pink_Idle_3.png").convert_alpha()
+p2_idle_4 = pygame.image.load("assets/images/playertwo/idle/Pink_Idle_4.png").convert_alpha()
+player_two_idle = [p2_idle_1, p2_idle_2, p2_idle_3, p2_idle_4]
 
 # create two instances of the players
-player_1 = Player(200, 310, player_data, playeronespritesheet, playeronesteps)
-player_2 = Player(700, 310, player_data, playertwospritesheet, playertwosteps)
+player_1 = Player(1, 200, 310, player_data)
+player_2 = Player(2, 700, 310, player_data)
 
 # load BG music
 pygame.mixer.music.load("assets/sounds/music/fridaytheme.mp3")
@@ -66,6 +82,8 @@ pygame.time.delay(500)
 
 # game loop
 run = True
+frame_time = 300  # 300 milliseconds
+last_frame_update = pygame.time.get_ticks()
 
 while run:
     clock.tick(FPS)
@@ -75,11 +93,17 @@ while run:
     health_bar(player_2.health, 580, 20)
 
     player_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, player_2)
-    #player_2.move()
+    player_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, player_1)
 
     # draw players
     player_1.draw(screen)
     player_2.draw(screen)
+
+    current_time = pygame.time.get_ticks()
+    if current_time - last_frame_update > frame_time:
+        player_1.next_frame()
+        player_2.next_frame()
+        last_frame_update = current_time
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- player.py
+++ player.py
@@ -1,20 +1,18 @@
 import pygame
 import pygame.draw
-import spritesheet
 
 class Player():
-    def __init__(self, x, y, data, sprite_sheets: list, animation_steps):
+    def __init__(self, player, x, y, data):
+        self.player = player
         self.size = data
         self.image_scale = data[1]
         self.flip = False
+        self.offset = data[2]
         self.action = 0 # what the player doing
         # 0 is idle, 1 is run, 2 is jump, 3 the attack, 5 hit, 6 death
-        self.animation_list = sprite_sheets
         self.frame_index = 0
-        current_spritesheet = 0
         self.image = 0
         self.update_time = pygame.time.get_ticks()
-        self.frame_index = 0
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
@@ -22,23 +20,26 @@ class Player():
         self.attacking = False
         self.health = 100
 
-    def load_images(self, sprite_sheets, animation_steps):
-        animation_list = []
-        for sheet, steps in zip(sprite_sheets, animation_steps):
-            animation_frames = []
-            for frame in range(steps):
-                image = self.get_image(sheet, frame)
-                animation_frames.append(image)
-            animation_list.append(animation_frames)
-        return animation_list
+        # Load and scale images
+        self.player_one_idle = [
+            self.scale_image(pygame.image.load(f"assets/images/playerone/idle/Blue_Idle_{i}.png").convert_alpha())
+            for i in range(1, 6)
+        ]
 
-    def get_image(self, sheet, frame):
-        width = sheet.get_width() // 5
-        height = sheet.get_height()
-        image = pygame.Surface((width, height), pygame.SRCALPHA).convert_alpha()
-        image.blit(sheet, (0, 0), (frame * width, 0, width, height))
+        self.player_two_idle = [
+            self.scale_image(pygame.image.load(f"assets/images/playertwo/idle/Pink_Idle_{i}.png").convert_alpha())
+            for i in range(1, 5)
+        ]
 
-        return image
+        self.current_frame = 0
+
+    def scale_image(self, image):
+        return pygame.transform.scale(image,(image.get_width() * self.image_scale, image.get_height() * self.image_scale))
+    def get_image(self):
+        if self.player == 1:
+            return self.player_one_idle[self.current_frame]
+        if self.player == 2:
+            return self.player_two_idle[self.current_frame]
 
     def move(self, SCREEN_WIDTH, SCREEN_HEIGHT, surface, enemy):
         speed = 10
@@ -50,22 +51,41 @@ class Player():
 
         # check for movement and NOT when attacking
         if not self.attacking:
-            if key[pygame.K_a]:
-                self.action = 1  # Run
-                change_x = -speed
-            elif key[pygame.K_d]:
-                self.action = 1  # Run
-                change_x = speed
-            else:
-                self.action = 0  # Idle
+            if self.player == 1:
+                if key[pygame.K_a]:
+                    self.action = 1  # Run
+                    change_x = -speed
+                elif key[pygame.K_d]:
+                    self.action = 1  # Run
+                    change_x = speed
+                else:
+                    self.action = 0  # Idle
 
-            if key[pygame.K_w] and self.jump == False:
-                self.jump = True
-                self.vel_y = -30
+                if key[pygame.K_w] and self.jump == False:
+                    self.jump = True
+                    self.vel_y = -30
 
-            #attack
-            if key[pygame.K_f]:
-                self.attack(surface, enemy)
+                #attack
+                if key[pygame.K_f]:
+                    self.attack(surface, enemy)
+
+            if self.player == 2:
+                if key[pygame.K_LEFT]:
+                    self.action = 1  # Run
+                    change_x = -speed
+                elif key[pygame.K_RIGHT]:
+                    self.action = 1  # Run
+                    change_x = speed
+                else:
+                    self.action = 0  # Idle
+
+                if key[pygame.K_UP] and self.jump == False:
+                    self.jump = True
+                    self.vel_y = -30
+
+                #attack
+                if key[pygame.K_l]:
+                    self.attack(surface, enemy)
 
 
         self.vel_y += gravity
@@ -103,25 +123,22 @@ class Player():
 
         pygame.draw.rect(surface, (0, 0, 255), collision_attack)
 
-    def draw(self, surface):
-        #pygame.draw.rect(surface, (255, 0, 0), self.rect)
-        #surface.blit(self.image, (self.rect.x, self.rect.y))
-        self.update_animation()
-        flipped_image = pygame.transform.flip(self.image, self.flip, False)
-        surface.blit(flipped_image, (self.rect.x, self.rect.y))
+    def next_frame(self):
+        self.current_frame += 1
+        if self.current_frame == len(self.player_one_idle):
+            self.current_frame = 0
+        if self.current_frame == len(self.player_two_idle):
+            self.current_frame = 0
 
-    def update_animation(self):
-        animation_cooldown = 100  # Milliseconds
-        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
-            self.update_time = pygame.time.get_ticks()
-            self.frame_index += 1
-            current_spritesheet = self.animation_list[self.action]
-            if self.frame_index >= current_spritesheet.size[1]:
-                self.frame_index = 0
-                if self.action == 3:  # Attack animation ends
-                    self.attacking = False
-                    self.action = 0  # Back to idle
-            self.image = (self.animation_list[self.action]).image_at([(self.frame_index * 292), 0, (self.frame_index * 292) + 292, 292])
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255, 0, 0), self.rect)
+
+        #flipped_image = pygame.transform.flip(self.get_image(), self.flip, False)
+        #surface.blit(flipped_image, (self.rect.x, self.rect.y))
+
+        img = pygame.transform.flip(self.get_image(), self.flip, False)
+        surface.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
+
 
 
 
